75c6bb0c971c5d83604ecef9af25fd3c02eff063
gky <3061412170@qq.com>
2022-06-18 20:30:54 +0800

    完成走一走

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <template>
   <div id="app">
-
+    <ul>
+      <li v-for="item in add" :key="item">{{item}}</li>
+    </ul>
+    <button @click="box">走一走</button>
   </div>
+  
 </template>
 
 <script>
   export default{
-    
+    name:"add",
+    data () {
+      return {
+        add:["帅哥","美女","程序员"]
+      }
+    },
+    methods:{
+      box(){
+        this.add.push(this.add[0])
+        this.add.shift()
+      }
+    }
   }
 </script>
 
